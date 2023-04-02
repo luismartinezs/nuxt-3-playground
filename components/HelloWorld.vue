4307d1c2a799606349a78a8100421b5483a6c25c
Luis Martinez <luismartinezwebdev@gmail.com>
2023-04-02 12:04:50 +0100

    chore: first commit

---
+++ components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<script setup>
+const msg = ref("World")
+const greeting = computed(() => `Hello ${msg.value}!`)
+const count = ref(1)
+const double = computed(() => count.value * 2)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <p class="greeting">{{ greeting }}</p>
+  <p>
+    <button @click="count++">count is: {{ count }}</button>
+    <br>
+    <span>double of count is: {{ double }}</span>
+  </p>
+</template>
+
+<style>
+.greeting {
+  color: red;
+  font-weight: bold;
+}
+</style>
